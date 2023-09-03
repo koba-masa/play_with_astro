1a95e548b4c9cd89e54459e9cc8eeed4359bbb38
koba-masa <kobayashi_mshr@koba-masa.com>
2023-09-03 19:53:35 +0900

    fix: GETをgetに変更する

--- empty/src/pages/rss.xml.js
+++ empty/src/pages/rss.xml.js
@@ -1,6 +1,6 @@
 import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
-export async function GET() {
+export async function get() {
   return rss({
     title: 'Astro学習者 | ブログ',
     description: 'Astroを学ぶ旅',
